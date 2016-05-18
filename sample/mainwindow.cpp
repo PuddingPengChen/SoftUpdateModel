b138c77b7baf1d46c02f41440b7c4d5e926cc2a5
bingochan <805318580@qq.com>
2016-05-18 14:49:29 +0800

    first commit

---
+++ sample/mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "fvupdater.h"
+#include <QDebug>
+
+
+MainWindow::MainWindow(QWidget *parent) :
+	QMainWindow(parent),
+	ui(new Ui::MainWindow)
+{
+	ui->setupUi(this);
+
+	// Connect the "check for updates manually" button with the autoupdater
+	connect(ui->updateButton, SIGNAL(clicked()),
+			FvUpdater::sharedUpdater(), SLOT(CheckForUpdatesNotSilent()));
+}
+
+MainWindow::~MainWindow()
+{
+	delete ui;
+}
+
+void MainWindow::on_updateButton_clicked()
+{
+
+}
